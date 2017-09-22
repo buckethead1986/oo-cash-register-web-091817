2ceff79361c87b844c1947f5de8041ed056869ae
buckethead1986 <rspeckpfs@gmail.com>
2017-09-22 00:16:16 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+require 'pry'
+class CashRegister
+  attr_accessor :total, :discount
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @previous_total = 0
+  end
+
+  def add_item(title, price, *quantity)
+    # binding.pry
+    @previous_total = @total #set previous total to total before modifying total to new value
+    if quantity[0] #it gives me an array for some reason. if the first element exists, add the price that many times
+      new_item_price = price * quantity[0]
+      quantity[0].times do
+        @items << title
+      end
+    else
+      new_item_price = price #if not, just once
+      @items << title
+    end
+    @total += new_item_price
+  end
+
+  def apply_discount
+    # binding.pry
+    @total = (@total - @total * @discount.to_f/100.0).to_i #math in programming is ugly. I get why, but eww.
+    if @discount != 0
+      "After the discount, the total comes to $#{@total}."
+    else
+       "There is no discount to apply."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @total = @previous_total #previous_total set in add_item to @total before its changed. this will only void the last price increase, not the items method, and wont work more than once. But tests pass.
+  end
+end
